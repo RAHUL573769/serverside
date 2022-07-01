7796b983ba6ef9e8738cd0384e7a6599d9df6cd4
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-07-01 10:14:24 +0600

    changes commit

--- index.js
+++ index.js
@@ -14,18 +14,40 @@ app.get("/", (req, res) => {
 
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iuaz1.mongodb.net/?retryWrites=true&w=majority`;
-
 console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-});
+// client.connect((err) => {
+//   const todoCollection = client.db("endgame-assignment").collection("todo");
+// });
 
+async function run() {
+  try {
+    await client.connect();
+    const todoCollection = client.db("endgame-assignment").collection("todo");
+
+    app.post("/todo", async (req, res) => {
+      const newTodo = req.body;
+      console.log(newTodo);
+      const resultTodo = await todoCollection.insertOne(newTodo);
+
+      res.send(resultTodo);
+    });
+
+    app.get("/todo", async (req, res) => {
+      const query = {};
+      const cursor = todoCollection.find(query);
+      const todoFinal = await cursor.toArray();
+      res.send(todoFinal);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
